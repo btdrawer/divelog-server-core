0d5925eb68bd205bb9eb824bed576378276de5bd
Ben Drawer <benjamin.drawer@tray.io>
2020-05-04 23:41:11 +0100

    Add caching

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const resources = require("./constants/resources");
 const errorCodes = require("./constants/errorCodes");
 const authUtils = require("./authUtils");
-const db = require("./db");
+
+const db = require("./services/db");
+const cache = require("./services/cache");
+const redisClient = require("./services/redisClient");
 
 const UserModel = require("./models/UserModel");
 const DiveModel = require("./models/DiveModel");
@@ -14,6 +17,8 @@ module.exports = {
     errorCodes,
     authUtils,
     db,
+    cache,
+    redisClient,
     models: {
         UserModel,
         DiveModel,
