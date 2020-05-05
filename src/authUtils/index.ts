28bf79a090adc17004ca80bfe32c353b84382530
Ben Drawer <benjamin.drawer@tray.io>
2020-05-05 21:27:45 +0100

    token does not need to be returned from getAuthData

--- src/authUtils/index.ts
+++ src/authUtils/index.ts
@@ -14,7 +14,7 @@ export const getAuthData = (req: Request): any => {
             const token = header.replace("Bearer ", "");
             const data = jwt.verify(token, <string>process.env.JWT_KEY);
 
-            return { token, data };
+            return data;
         }
     }
     return null;
@@ -22,7 +22,7 @@ export const getAuthData = (req: Request): any => {
 
 export const getUserId = (req: Request): string => {
     const authData: any = getAuthData(req);
-    return authData ? authData.data.id : null;
+    return authData ? authData.id : null;
 };
 
 export const signJwt = (id: string): string =>
