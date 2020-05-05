0fef119aba2ccc8088b45bc0088e963404471693
Ben Drawer <benjamin.drawer@tray.io>
2020-05-05 20:44:15 +0100

    Proper TypeScript exports

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as resources from "./constants/resources";
 import * as errorCodes from "./constants/errorCodes";
 import * as subscriptionKeys from "./constants/subscriptionKeys";
 
-import * as authUtils from "./authUtils";
+import * as auth from "./authUtils";
 
 import db from "./services/db";
 import cache from "./services/cache";
@@ -14,19 +14,21 @@ import GearModel from "./models/GearModel";
 import ClubModel from "./models/ClubModel";
 import GroupModel from "./models/GroupModel";
 
-module.exports = {
+const models = {
+    UserModel,
+    DiveModel,
+    GearModel,
+    ClubModel,
+    GroupModel,
+};
+
+export {
     resources,
     errorCodes,
     subscriptionKeys,
     db,
     cache,
     redisClient,
-    auth: authUtils,
-    models: {
-        UserModel,
-        DiveModel,
-        GearModel,
-        ClubModel,
-        GroupModel,
-    },
+    auth,
+    models,
 };
